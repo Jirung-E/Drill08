5599c45a6ba674e98fb2d0b2d74773e1430e8e3b
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-10-05 13:04:18 +0900

    공이 떨어짐

---
+++ boy_grass_object.py
@@ -0,0 +1,106 @@
+from pico2d import *
+import random
+
+# Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x = random.randint(100, 700)
+        self.y = 90
+        self.vx = random.randint(3, 7)
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        # self.x += self.vx
+        self.x += 5
+
+
+class Ball:
+    def __init__(self):
+        self.x = random.randint(50, 750)
+        self.y = 599
+        self.vy = random.randint(30, 70) / 10
+        self.frame = random.randint(0, 7)
+        if random.randint(0, 1) == 0:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.y -= self.vy
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global balls
+    global world
+
+    running = True
+    grass = Grass()
+    team = [ Boy() for _ in range(11) ]
+    balls = [ Ball() for _ in range(20) ]
+    
+    world = []
+    world.append(grass)
+    world += team
+    world += balls
+
+
+def render_world():
+    clear_canvas()
+    for e in world:
+        e.draw()
+    update_canvas()
+
+
+def update_world():
+    for e in world:
+        e.update()
+
+
+open_canvas()
+
+# initialization code
+reset_world()
+
+# game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+# finalization code
+
+close_canvas()
